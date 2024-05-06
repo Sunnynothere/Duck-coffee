bfbdb684b270a872ef250443672ddcce4ed1380f
Sol <ususario@iMac-de-Ususario.local>
2024-05-06 18:22:34 -0300

    responsive and read more left

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Nav responsive:
+
 document.querySelector(".menu-btn").addEventListener("click", () => {
    document.querySelector(".nav-menu").classList.toggle("show");
 });
@@ -11,3 +13,14 @@ window.scrollTo({ top: document.getElementById("typeOfRoast").offsetTop });
 window.scrollTo({ top: document.getElementById("brewMethod").offsetTop });
 
 window.scrollTo({ top: document.getElementById("coffeeByCountries").offsetTop });
+
+// Read more:
+
+const spanElement = document.getElementById('span');
+const infoCountryElement = document.querySelector('.info_country');
+
+spanElement.addEventListener('click', function() {
+  infoCountryElement.classList.toggle('active');
+  spanElement.textContent = infoCountryElement.classList.contains('active') ? 'show less' : 'show more';
+});
+
